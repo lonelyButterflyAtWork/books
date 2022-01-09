7b3ad302d7760e5573d7583b85ebb5a1e6fd0b72
Gabriela <walczyk.gabriela2@gmail.com>
2022-01-09 11:50:59 +0100

    models and migrations

---
+++ app/Models/Book.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        "title",
+        "publication_year",
+        "isbn",
+    ];
+
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function publisher()
+    {
+        return $this->belongsTo(Publisher::class);
+    }
+}
---
+++ database/migrations/2022_01_09_100717_create_books_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('author_id');
+            $table->text('title');
+            $table->integer('isbn');
+
+            $table->foreign('author_id')->references('id')->on('authors');
+        });
+
+        DB::statement('ALTER TABLE books MODIFY COLUMN ISBN INTEGER (13);');
+
+        Schema::table('books', function($table) {
+            $table->foreignId('publisher_id');
+            $table->foreign('publisher_id')->references('id')->on('publishers');
+            $table->year('publication_year');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
