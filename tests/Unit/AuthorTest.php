f8d067f33e57d9973685a6f38bd6cbc2400da6fc
Gabriela <walczyk.gabriela2@gmail.com>
2022-01-10 23:08:43 +0100

    author tests

---
+++ tests/Unit/AuthorTest.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use App\Models\Author;
+use PHPUnit\Framework\TestCase;
+
+class AuthorTest extends TestCase
+{
+
+    private $author;
+
+    public function __construct()
+    {
+        $this->autor = new Author();
+        parent::__construct();
+    }
+
+    public function testIfTimestampsIsFalse()
+    {
+        $this->assertClassHasAttribute('fillable', Author::class);
+        $this->assertEquals($this->autor->fillable, false);
+    }
+}
